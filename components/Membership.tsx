f608fe2427f6b70f98471fb8672c8513a8a1acc4
zuhedshaikh95 <zuhed95@gmail.com>
2023-04-30 23:42:28 +0530

    add: Account membership component

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -10,7 +10,7 @@ const Membership = () => {
 	const [billingLoading, setBillingLoading] = useState<boolean>(false);
 
 	const manageSubscription = () => {
-		if(subscription) {
+		if (subscription) {
 			setBillingLoading(true);
 			goToBillingPortal();
 		}
@@ -26,6 +26,36 @@ const Membership = () => {
 				>
 					{billingLoading ? <Loader /> : "Cancel Membership"}
 				</button>
+			</div>
+
+			<div className="col-span-3">
+				<div className="flex flex-col justify-between border-b border-white/10 py-4 md:flex-row">
+					<div>
+						<p className="font-medium">{user?.email}</p>
+						<p className="text-[gray]">Password: ********</p>
+					</div>
+					<div className="md:text-right">
+						<p className="membershipLink">Change email</p>
+						<p className="membershipLink">Change password</p>
+					</div>
+				</div>
+
+				<div className="flex flex-col justify-between pt-4 pb-4 md:flex-row md:pb-0">
+					<div>
+						<p>
+							{subscription?.cancel_at_period_end
+								? "Your membership will end on"
+								: "Your next billing date is"}
+							{subscription?.current_period_end}
+						</p>
+					</div>
+					<div className="md:text-right">
+						<p className="membershipLink">Manage payment info</p>
+						<p className="membershipLink">Add backup payment method</p>
+						<p className="membershipLink">Billing details</p>
+						<p className="membershipLink">Change billing day</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
