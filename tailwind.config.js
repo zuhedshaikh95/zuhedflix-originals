69cc208670ff20cff3eb3ce4fa96d174e9a5cabe
zuhedshaikh95 <zuhed95@gmail.com>
2023-04-30 23:50:53 +0530

    fix: Large screen gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-to-b':
-          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 72%,#141414 100%);',
+          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
       }
     }
   },
