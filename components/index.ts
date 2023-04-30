4d845b7c3a884d90e544993ea7f9523ab9c91ba1
zuhedshaikh95 <zuhed95@gmail.com>
2023-04-30 23:41:45 +0530

    add: Mobile menu export

--- components/index.ts
+++ components/index.ts
@@ -7,3 +7,4 @@ export { default as Plans } from './Plans';
 export { default as Table } from './Table';
 export { default as Loader } from './Loader';
 export { default as Membership } from './Membership';
+export { default as BasicMenu } from './BasicMenu';
