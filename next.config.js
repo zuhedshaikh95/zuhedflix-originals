9e6a41ad72d20cb95ce303950d3c6a4834d6f64f
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-01 13:52:57 +0530

    add: secured keys to .env

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
   transpilePackages: ['@stripe/firestore-stripe-payments']
 }
